c006130d03dea6806e71190e72ef1f20343cd573
prudencerich <maryejiropraise@gmail.com>
2023-04-04 14:46:29 -0700

    functions way

---
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * set_string - entry point of the program
+ *
+ * @s: input
+ *
+ * @to: input
+ *
+ * Return: 0 always (success)
+ */
+
+void set_string(char **s, char *to)
+{
+	*s = to;
+}
